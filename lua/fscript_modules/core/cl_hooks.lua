fecf7b6225e009f4c5ca551b4f71561cd1306736
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2021-03-18 17:07:50 +0100

    Update door check from the DarkRP one
    Source: https://github.com/FPtje/DarkRP/blob/42f9f75871321ef337c2d91f41958950c58aff35/entities/weapons/keys/cl_menu.lua#L49

--- lua/fscript_modules/core/cl_hooks.lua
+++ lua/fscript_modules/core/cl_hooks.lua
@@ -3,8 +3,9 @@
 -- Source : https://github.com/FPtje/DarkRP/blob/db3bfc6e838fbeba7efcb6732a342a16707089a6/entities/weapons/keys/cl_menu.lua#L49
 hook.Add("FScript.CanOpenPlayerMenu", "FScript.PlayerMenuRestrictions", function(ply, name)
 	if FScript.Config.CommandMenuKey == KEY_F2 and name == "ChatCommands" then
-		local EntityTrace = ply:GetEyeTrace().Entity
-		if IsValid(EntityTrace) and EntityTrace:isKeysOwnable() and EntityTrace.HitPos:DistToSqr(EntityTrace:GetPos()) <= 40000 then
+		local trace = ply:GetEyeTrace()
+		local entity = trace.Entity
+		if IsValid(entity) and entity:isKeysOwnable() and trace.HitPos:DistToSqr(ply:EyePos()) <= 40000 then
 			return false
 		end
 	end
